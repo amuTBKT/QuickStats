99a2cc1969dd0f91261db75d8dbadbeaf40c61ab
Amit Kumar Mehar <gookutbkt@gmail.com>
2023-11-03 17:15:15 +0530

    Added stat preset factory

---
+++ Source/StatsVisualizerEditor/Private/StatPresetFactory.cpp
@@ -0,0 +1,17 @@
+// Copyright 2023 Amit Kumar Mehar. All Rights Reserved.
+
+#include "StatPresetFactory.h"
+#include "StatsVisualizerSettings.h"
+
+UCustomStatPresetFactory::UCustomStatPresetFactory(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	bCreateNew = true;
+	SupportedClass = UCustomStatPreset::StaticClass();
+}
+
+UObject* UCustomStatPresetFactory::FactoryCreateNew(UClass* Class, UObject* InParent, FName Name, EObjectFlags Flags, UObject* Context, FFeedbackContext* Warn)
+{
+	return NewObject<UCustomStatPreset>(InParent, Class, Name, Flags);
+}
+
---
+++ Source/StatsVisualizerEditor/Private/StatPresetFactory.h
@@ -0,0 +1,15 @@
+// Copyright 2023 Amit Kumar Mehar. All Rights Reserved.
+
+#pragma once
+
+#include "Factories/Factory.h"
+#include "StatPresetFactory.generated.h"
+
+UCLASS(HideCategories = Object, MinimalAPI)
+class UCustomStatPresetFactory : public UFactory
+{
+	GENERATED_UCLASS_BODY()
+
+	// UFactory interface
+	virtual UObject* FactoryCreateNew(UClass* Class, UObject* InParent, FName Name, EObjectFlags Flags, UObject* Context, FFeedbackContext* Warn) override;
+};
